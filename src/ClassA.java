516f59a26640c95f278dbc0e81552eba5e89c8cf
DmitriyPershukov <dmitriypershukov@gmail.com>
2024-08-08 13:09:25 +0500

    implemented doA

---
+++ src/ClassA.java
@@ -0,0 +1,13 @@
+public class ClassA {
+     int a;
+     int b;
+
+    public ClassA(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void doA(int multiplier){
+         a *= multiplier;
+     }
+}
